907f2f2a3f53cbfc23d50aec15c4ba7a0873a108
muhammadamran <amranhakim9@gmail.com>
2024-09-29 23:18:34 +0700

    Initial commit

--- pages/point-of-sales/new-orders.php
+++ pages/point-of-sales/new-orders.php
@@ -31,8 +31,73 @@
     <!-- End Page Header -->
     <div id="kt_app_content" class="app-content flex-column-fluid">
         <div id="kt_app_content_container" class="app-container container-fluid">
-            <!-- Begin -->
-            <!-- End -->
+            <div class="card">
+                <div class="button-card">
+                    <div class="for-export">
+                        <div class="for-export-2">
+                            <font class="font-export">Export File:&nbsp;</font>
+                            <a href="#" class="btn btn-outline btn-outline-dashed btn-outline-secondary card-toolbar font-export" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-trigger="hover" title="Click to download Excel file">Excel</a>
+                            <a href="#" class="btn btn-outline btn-outline-dashed btn-outline-secondary card-toolbar font-export" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-trigger="hover" title="Click to download PDF file">PDF</a>
+                        </div>
+                    </div>
+                    <div class="for-add">
+                        <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#kt_modal_create">
+                            <span class="svg-icon svg-icon-3">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                    <rect opacity="0.3" x="2" y="2" width="20" height="20" rx="10" fill="currentColor" />
+                                    <rect x="10.8891" y="17.8033" width="12" height="2" rx="1" transform="rotate(-90 10.8891 17.8033)" fill="currentColor" />
+                                    <rect x="6.01041" y="10.9247" width="12" height="2" rx="1" fill="currentColor" />
+                                </svg>
+                            </span>
+                            New Clients
+                        </a>
+                    </div>
+                </div>
+                <div class="card-body pt-0">
+                    <div class="table-responsive">
+                        <table class="table align-middle table-row-dashed fs-6 gy-5" id="">
+                            <thead>
+                                <tr class="text-start text-gray-400 fw-bold fs-7 text-uppercase gs-0">
+                                    <th>#</th>
+                                    <th class="min-w-20px">No.Refernce</th>
+                                    <th class="min-w-20px">Order<font style="color:transparent">.</font>Type</th>
+                                    <th class="min-w-20px">Customer</th>
+                                    <th class="min-w-20px">Service</th>
+                                    <th class="min-w-20px">Level</th>
+                                    <th class="min-w-10px" style="text-align: center;">
+                                        <font style="color:transparent">....</font>Status
+                                    </th>
+                                    <th class="text-end min-w-150px datatable-nosort">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody class="fw-semibold text-gray-600">
+                                <?php $dataquery = $db->query("SELECT * FROM clients WHERE is_deleted='false' ORDER BY client_id DESC"); ?>
+                                <?php
+                                if (mysqli_num_rows($dataquery) > 0) {
+                                    $no = 0;
+                                    while ($row = mysqli_fetch_array($dataquery)) {
+                                        $no++;
+                                ?>
+                                    <?php } ?>
+                                <?php } else { ?>
+                                    <tr>
+                                        <td colspan="8">
+                                            <center>
+                                                <div style="display: grid;">
+                                                    <div>
+                                                        <img src="assets/svg/no-data.svg" alt="No Data" style="width:30%">
+                                                    </div>
+                                                    <font class="fw-bolder text-muted">No data in this table</font>
+                                                </div>
+                                            </center>
+                                        </td>
+                                    </tr>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
